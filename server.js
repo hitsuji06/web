82621f62a4cdeb0504afa9f2ca561c438de15f60
hitsuji06 <fighterjr1@hotmail.com>
2024-04-09 23:02:07 -0600

    working on the db

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const mongoose =require('mongoose')
 const articleRouter= require('./routes/articles')
 const app= express()
 
-
+mongoose.connect('mongodb://localhost/blog')
 app.set('view engine','ejs')
 app.use('/articles', articleRouter)
 
